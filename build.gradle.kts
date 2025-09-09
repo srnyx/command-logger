6e6d6c6e3af58319d28c1ff0f4115a8853ec067d
srnyx <25808801+srnyx@users.noreply.github.com>
2025-09-09 14:07:24 -0400

    Update dependencies
    Gradle: 8.14.2 -> 9.0.0
    Gradle Galaxy: 1.3.3 -> 1.3.5
    Shadow: 8.3.6 -> 8.3.8
    Annoying API: b717656b2d -> 1c2e7eef30

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ import xyz.srnyx.gradlegalaxy.utility.setupAnnoyingAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.3.3"
-    id("com.gradleup.shadow") version "8.3.6"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.5"
+    id("com.gradleup.shadow") version "8.3.8"
 }
 
 paper("1.8.8")
-setupAnnoyingAPI("b717656b2d", "com.srnyx", "1.0.0", "Log commands executed by players and console to one file or multiple files")
+setupAnnoyingAPI("1c2e7eef30", "com.srnyx", "1.0.0", "Log commands executed by players and console to one file or multiple files")
